c606f5f5056ef351a25593bf7719cebe6c156eea
wayne2k <wayne2k@gmail.com>
2015-02-11 15:50:14 +0200

    Basic Board Setup
    Prefabs created.
    Board and game manager scripts created.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance = null;
+
+	public BoardManager boardScript;
+
+	private int level = 3;
+
+	void Awake ()
+	{
+		if (Instance == null)
+			Instance = this;
+		else if (Instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		boardScript = GetComponent <BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame ()
+	{
+		boardScript.SetupScene (level);
+	}
+
+}
